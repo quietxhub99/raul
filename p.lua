254a9647dc5665b68e73b7bf58790da25060529b
quietxhub99 <hubquiet199@gmail.com>
2025-08-19 01:52:24 +0700

    Update p.lua

--- p.lua
+++ p.lua
@@ -1,6 +1,6 @@
 -- SUKIJAR MAKAN DI KALI
 -- AKU KEJAR, JANGAN LARI WLEE
 
-loadstring(game:HttpGet("https://pastebin.com/raw/BCDgBqct"))()
+loadstring(game:HttpGet("https://dpaste.com/8QHT7JPBF.txt"))()
 
 -- SINI SAYANGKU HAHAHA

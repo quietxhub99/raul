f276b317f679150491937eacf3f84c67789ab0d6
quietxhub99 <hubquiet199@gmail.com>
2025-08-18 18:15:46 +0700

    Create p.lua

---
+++ p.lua
@@ -0,0 +1,6 @@
+-- SUKIJAR MAKAN DI KALI
+-- AKU KEJAR, JANGAN LARI WLEE
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/quietxhub99/raul/refs/heads/fish/Permanen.lua"))()
+
+-- SINI SAYANGKU HAHAHA
